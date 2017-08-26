facef8e14478126b698e66ff0331f3a9f0d2ee52
matthiashaefeli <haefeli.matthias@gmail.com>
2017-08-25 23:05:33 -0500

    add palindrome tests

--- spec/javascripts/practice_spec.js
+++ spec/javascripts/practice_spec.js
@@ -3,3 +3,15 @@ describe('factorial', function() {
 		expect(factorial(5)).toEqual(120);
 	});
 });
+
+describe('palindrome', function() {
+	it('return false if the word is a palindrome.', function() {
+		expect(palindrome('hello')).toEqual(false);
+	});
+	it('return true if the given word is a palindrome.', function() {
+		expect(palindrome('eye')).toEqual(true);
+	});
+	it('return true if the given word is a palindrom(delete all numbers and signs)', function() {
+		expect(palindrome('&**(+_ 87y8ojo((')).toEqual(true);
+	});
+});
