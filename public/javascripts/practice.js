63565df255a36e561d3f6a4ca3b288a4467ddaaa
matthiashaefeli <haefeli.matthias@gmail.com>
2017-08-25 22:52:36 -0500

    add factorial test and solution

---
+++ public/javascripts/practice.js
@@ -0,0 +1,8 @@
+function factorial(num) {
+	var result = 1;
+	for (var i = 1; i < num + 1; i++) {
+		result *= i;
+	}
+
+	return result;
+}
---
+++ spec/javascripts/practice_spec.js
@@ -0,0 +1,5 @@
+describe('factorial', function() {
+	it('Return the factorial of the provided integer.', function() {
+		expect(factorial(5)).toEqual(120);
+	});
+});
