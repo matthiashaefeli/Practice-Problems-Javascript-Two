5975bd1bd2049602433cd642503500701f09f9a2
matthiashaefeli <haefeli.matthias@gmail.com>
2017-08-26 11:32:21 -0500

    add tests for ends with

--- spec/javascripts/practice_spec.js
+++ spec/javascripts/practice_spec.js
@@ -58,3 +58,20 @@ describe('largestOfFour', function() {
 		]);
 	});
 });
+
+describe('confirmEnding', function() {
+	it('returns true if a given string (first argument) ends with the letter of the second argument', function() {
+		expect(confirmEnding('Bastian', 'n')).toEqual(true);
+	});
+	it('returns true if a given string (first argument) ends with the letter of the second argument', function() {
+		expect(confirmEnding('He has to give me a new name', 'name').toEqual(true));
+	});
+	it('returns false if a given string (first argument) not ends with the letter of the second argument', function() {
+		expect(
+			confirmEnding(
+				'Walking on water and developing software from a specification are easy if both are frozen',
+				'specification'
+			).toEqual(false)
+		);
+	});
+});
