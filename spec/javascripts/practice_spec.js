b32f1e43a70a67fd506d9c3c519415fd04c455d8
matthiashaefeli <haefeli.matthias@gmail.com>
2017-08-28 11:21:06 -0500

    add solution for chunkArrayInGroups

--- spec/javascripts/practice_spec.js
+++ spec/javascripts/practice_spec.js
@@ -119,6 +119,6 @@ describe('chunkArrayInGroups', function() {
 		expect(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3)).toEqual([[0, 1, 2], [3, 4, 5]]);
 	});
 	it('splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array', function() {
-		expect(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4).toEqual([[0, 1, 2, 3], [4, 5, 6, 7], [8]]));
+		expect(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4)).toEqual([[0, 1, 2, 3], [4, 5, 6, 7], [8]]);
 	});
 });
