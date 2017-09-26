0c1c8cddbdd71d4790da108818ebb1f6ebc21bbd
matthiashaefeli <haefeli.matthias@gmail.com>
2017-09-25 19:13:23 -0500

    add tests for uniteUnique

--- spec/javascripts/practice_spec.js
+++ spec/javascripts/practice_spec.js
@@ -335,3 +335,18 @@ describe('booWho', function() {
 		expect(booWho(NaN)).toEqual(false)
 	})
 })
+
+describe('uniteUnique', function() {
+	it('new array of unique values in the order of the original provided arrays', function() {
+		expect(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1])).toEqual([1, 3, 2, 5, 4])
+	})
+	it('new array of unique values in the order of the original provided arrays', function() {
+		expect(uniteUnique([1, 3, 2], [1, [5]], [2, [4]])).toEqual([1, 3, 2, [5], [4]])
+	})
+	it('new array of unique values in the order of the original provided arrays', function() {
+		expect(uniteUnique([1, 2, 3], [5, 2, 1])).toEqual([1, 2, 3, 5])
+	})
+	it('new array of unique values in the order of the original provided arrays', function() {
+		expect(uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8])).toEqual([1, 2, 3, 5, 4, 6, 7, 8])
+	})
+})
